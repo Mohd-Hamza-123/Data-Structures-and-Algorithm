44c64985f83b1f6f2a2ac4dcb2cd173b08720b0a
Mohd Hamza <mohdhamza.me@gmail.com>
2024-05-24 20:17:35 +0530

    commit

--- DSA in JavaScript/Basic Problems/Reverse Every Word in String.js
+++ DSA in JavaScript/Basic Problems/Reverse Every Word in String.js
@@ -3,6 +3,7 @@ let reverseSentence = "";
 
 let x = "";
 let y = "";
+
 for (let i = 0; i < sentence.length; i++) {
 
 
@@ -23,64 +24,3 @@ for (let i = 0; i < sentence.length; i++) {
 
 console.log(reverseSentence)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let sentence = "Mohd Hamza Ansari 123";
-// let reverseSentence = "";
-
-// let x = "";
-// let y = "";
-// for (let i = 0; i < sentence.length; i++) {
-//   // Build the word until a space or the last character
-//   if (sentence[i] !== " " && i !== sentence.length - 1) {
-//     x += sentence[i];
-//   } else {
-//     if (i === sentence.length - 1) x += sentence[i]; // Include the last character in x
-    
-//     // Reverse the word stored in x and store it in y
-//     for (let j = 0; j < x.length; j++) {
-//       y += x[x.length - 1 - j];
-//     }
-    
-//     // Add a space before the word if it's not the first word
-//     if (reverseSentence.length > 0) {
-//       reverseSentence += " ";
-//     }
-//     reverseSentence += y;
-    
-//     // Reset x and y for the next word
-//     y = "";
-//     x = "";
-//   }
-// }
-
-// console.log(reverseSentence);
